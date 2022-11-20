d6ca71942185c9f87544ff540c275b9103720578
Shamsad Anam <shamsad.anam@gmail.com>
2022-11-20 21:38:11 +0600

    fc

---
+++ owl/controls.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    margin: 10,
+    nav: false,
+    loop: true,
+    items: 1,
+    autoplay: false,
+    autoplaySpeed: 1000,
+    autoplayTimeout: 3000,
+  });
+});
